40525977580148e7742d41330c9c156f10bc4c1a
Suhun0331 <rlatngns6046@naver.com>
2025-01-22 09:38:39 +0900

    Add CalendarTest & Week Enum

---
+++ src/main/java/org/example/jan/java_250120/ch05/CalendarTest.java
@@ -0,0 +1,8 @@
+package org.example.jan.java_250120.ch05;
+
+public class ArgsinputTest {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/org/example/jan/java_250120/ch05/Week.java
@@ -0,0 +1,2 @@
+package org.example.jan.java_250120.ch05;public enum Week {
+}
